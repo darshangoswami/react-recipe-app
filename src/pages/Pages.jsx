9b12acbf76faa0b3dc222398251ce41cc72bb4f5
Darshan Goswami <dexter@Darshans-MacBook-Air.local>
2022-08-08 20:41:32 +0530

    feat: Add search bar to search for food items

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 import { Route, Routes } from "react-router-dom";
+import Searched from "./Searched";
 
 function Pages() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cuisine/:type" element={<Cuisine />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
+    </Routes>
   );
 }
 
