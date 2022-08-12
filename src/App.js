531292f52a6c5762c2d1cfca5913e13b98ed209a
Darshan Goswami <dexter@Darshans-MacBook-Air.local>
2022-08-13 01:19:40 +0530

    feat: Add Logo in the top bar to navigate back to home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
-import { BrowserRouter } from "react-router-dom";
+import { Link, BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Logo to={"/"}>
+          <GiKnifeFork />
+          <h2>Tastyyy</h2>
+        </Logo>
+
         <Search />
         <Category />
         <Pages />
@@ -14,5 +21,30 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 400;
+    font-family: "Lobster Two", cursive;
+  }
+  text-decoration: none;
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
+// const Nav = styled.div`
+//   padding: 4rem 0;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+//   svg {
+//     font-size: 2rem;
+//   }
+// `;
 
 export default App;
